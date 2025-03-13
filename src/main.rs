4da174da5386ee9e5005b6d4bb61c3d2cc0d430b
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-13 14:21:37 +0000

    test: write tests for cli

--- src/main.rs
+++ src/main.rs
@@ -44,45 +44,45 @@ async fn process_subcommand(subcommand: &str, args: CommandArgs) -> Result<(), B
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let args = cli::Cli::parse(env::args())?;
+async fn main() -> anyhow::Result<()> {
+    // let args = cli::Cli::parse(env::args())?;
 
-    if args.help || args.subcommand.is_none() {
-        todo!()
-    } else if args.version {
-        print!("{}", env!("CARGO_PKG_VERSION"))
-    } else {
-        match args.subcommand.unwrap() {
-            Subcommand::Init => (),
-        }
-    }
-
-    Ok(())
-
-    // let mut args = env::args();
-    // let _command_name = args.next();
-    // let subcommand = args.next().unwrap_or_default();
-
-    // let mut args: CommandArgs = args.collect();
-
-    // if subcommand.starts_with('-') {
-    //     // We're not using any command, only flags
-    //     args.insert(subcommand.clone());
-    // }
-
-    // if HELP_FLAG.is_in(&args) {
-    //     help(Some(&subcommand))
-    // } else if VERSION_FLAG.is_in(&args) {
-    //     print!("{}", env!("CARGO_PKG_VERSION"));
-
-    //     Ok(())
+    // if args.help || args.subcommand.is_none() {
+    //     todo!()
+    // } else if args.version {
+    //     print!("{}", env!("CARGO_PKG_VERSION"))
     // } else {
-    //     match process_subcommand(subcommand.as_str(), args).await {
-    //         Ok(()) => Ok(()),
-    //         Err(msg) => {
-    //             fail!("{msg}");
-    //             process::exit(1);
-    //         }
+    //     match args.subcommand.unwrap() {
+    //         Subcommand::Init => (),
     //     }
     // }
+
+    // Ok(())
+
+    let mut args = env::args();
+    let _command_name = args.next();
+    let subcommand = args.next().unwrap_or_default();
+
+    let mut args: CommandArgs = args.collect();
+
+    if subcommand.starts_with('-') {
+        // We're not using any command, only flags
+        args.insert(subcommand.clone());
+    }
+
+    if HELP_FLAG.is_in(&args) {
+        help(Some(&subcommand))
+    } else if VERSION_FLAG.is_in(&args) {
+        print!("{}", env!("CARGO_PKG_VERSION"));
+
+        Ok(())
+    } else {
+        match process_subcommand(subcommand.as_str(), args).await {
+            Ok(()) => Ok(()),
+            Err(msg) => {
+                fail!("{msg}");
+                process::exit(1);
+            }
+        }
+    }
 }
